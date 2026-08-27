b34dd8b47abe1a99f72c1b574cad4834dd6d74e3
Brandon Gable <brandonryangable@gmail.com>
2026-08-26 22:34:42 -0400

    HTML and CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -11,6 +12,40 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload Hello World.
             </p>
+            <div
+                style={{
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "red",
+                }}
+            >
+                <Container>
+                    <Row>
+                        <Col>
+                            <h1>Hi</h1>
+                        </Col>
+                        <Col>
+                            <Button
+                                onClick={() => {
+                                    console.log("Hello World!");
+                                }}
+                            >
+                                Log Hello World
+                            </Button>
+                        </Col>
+                        <Col>
+                            <img src="./crow.jpeg" alt="Crow" />
+                        </Col>
+                        <Col>
+                            <ol>
+                                <li>Tubas</li>
+                                <li>Baritones</li>
+                                <li>Trombones</li>
+                            </ol>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }

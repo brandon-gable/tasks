0565c79df1a0d5afb59b6baa384cb1e3c09cec24
Brandon Gable <brandonryangable@gmail.com>
2026-09-06 14:55:52 -0400

    completed allRGB

--- src/arrays.ts
+++ src/arrays.ts
@@ -77,7 +77,12 @@ export function countShortWords(words: string[]): number {
  * then return true.
  */
 export function allRGB(colors: string[]): boolean {
-    return false;
+    return !colors.length ? true : (
+            colors.every(
+                (color: string): boolean =>
+                    color === "red" || color === "blue" || color === "green",
+            )
+        );
 }
 
 /**

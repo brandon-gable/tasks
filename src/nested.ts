322c9eab87d0803968a9878f946e5a922129bc5b
Brandon Gable <brandonryangable@gmail.com>
2026-09-09 12:20:10 -0400

    sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -74,7 +74,14 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedQuestions: Question[] = questions.filter(
+        (question: Question): boolean => question.published,
+    );
+    return publishedQuestions.reduce(
+        (currentSum: number, publishedQuestion: Question): number =>
+            currentSum + publishedQuestion.points,
+        0,
+    );
 }
 
 /***

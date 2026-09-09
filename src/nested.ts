651614dcd1ff8008cb4b9f51c3fca98e28823595
Brandon Gable <brandonryangable@gmail.com>
2026-09-09 12:15:50 -0400

    sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -63,7 +63,11 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    return questions.reduce(
+        (currentSum: number, question: Question): number =>
+            currentSum + question.points,
+        0,
+    );
 }
 
 /***

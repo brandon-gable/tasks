fb4b885aab8f4a52b378da95d5b9b641128c252c
Brandon Gable <brandonryangable@gmail.com>
2026-09-09 21:30:34 -0400

    completed toCSV

--- src/nested.ts
+++ src/nested.ts
@@ -102,7 +102,13 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return "";
+    const stringQuestions: string = questions
+        .map(
+            (question: Question): string =>
+                `${question.id},${question.name},${question.options.length},${question.points},${question.published}`,
+        )
+        .join("\n");
+    return `id,name,options,points,published\n${stringQuestions}`;
 }
 
 /**

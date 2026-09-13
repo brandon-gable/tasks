6dca6e4b029f73a2cd6bd873c8f8f081993b471e
Brandon Gable <brandonryangable@gmail.com>
2026-09-13 14:20:40 -0400

    copmpleted sameType

--- src/nested.ts
+++ src/nested.ts
@@ -142,7 +142,17 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    return (
+        questions.length === 0 ? true
+        : (
+            questions.filter(
+                (question: Question): boolean =>
+                    question.type !== questions[0].type,
+            ).length > 0
+        ) ?
+            false
+        :   true
+    );
 }
 
 /***

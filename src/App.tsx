566f3456bbefa242d7c349918f97b4a50486ceda
Brandon Gable <brandonryangable@gmail.com>
2026-09-13 15:03:06 -0400

    accepted incoming merge in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import React from "react";
 import "./App.css";
-<<<<<<< HEAD
-import { Button, Col, Container, Row } from "react-bootstrap";
-=======
 import { ChangeType } from "./components/ChangeType";
 import { RevealAnswer } from "./components/RevealAnswer";
 import { StartAttempt } from "./components/StartAttempt";
 import { TwoDice } from "./components/TwoDice";
 import { CycleHoliday } from "./components/CycleHoliday";
 import { Counter } from "./components/Counter";
->>>>>>> upstream/task-state
 
 function App(): React.JSX.Element {
     return (
@@ -17,46 +13,6 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript (Brandon Gable)
             </header>
-<<<<<<< HEAD
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload Hello World.
-            </p>
-            <div
-                style={{
-                    width: "100%",
-                    height: "100%",
-                    backgroundColor: "red",
-                }}
-            >
-                <Container>
-                    <Row>
-                        <Col>
-                            <h1>Hi</h1>
-                        </Col>
-                        <Col>
-                            <Button
-                                onClick={() => {
-                                    console.log("Hello World!");
-                                }}
-                            >
-                                Log Hello World
-                            </Button>
-                        </Col>
-                        <Col>
-                            <img src="./crow.jpeg" alt="Crow" />
-                        </Col>
-                        <Col>
-                            <ol>
-                                <li>Tubas</li>
-                                <li>Baritones</li>
-                                <li>Trombones</li>
-                            </ol>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-=======
             <hr></hr>
             <Counter></Counter>
             <hr />
@@ -69,7 +25,6 @@ function App(): React.JSX.Element {
             <ChangeType></ChangeType>
             <hr />
             <CycleHoliday></CycleHoliday>
->>>>>>> upstream/task-state
         </div>
     );
 }

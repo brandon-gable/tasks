5147df7860542b257e38bc7cf36e8bdf1448ce1a
Brandon Gable <brandonryangable@gmail.com>
2026-09-13 18:39:27 -0400

    completed CycleHoliday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,49 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    enum Holiday {
+        Hanukkah = "🕎",
+        Birthday = "🎂",
+        Halloween = "🎃",
+        NewYears = "🎆",
+        Thanksgiving = "🦃",
+    }
+
+    const [holiday, setHoliday] = useState<Holiday>(Holiday.Halloween);
+
+    const ALPHABET_TRANSITIONS: Record<Holiday, Holiday> = {
+        [Holiday.Halloween]: Holiday.NewYears,
+        [Holiday.NewYears]: Holiday.Thanksgiving,
+        [Holiday.Thanksgiving]: Holiday.Birthday,
+        [Holiday.Birthday]: Holiday.Hanukkah,
+        [Holiday.Hanukkah]: Holiday.Halloween,
+    };
+
+    const YEAR_TRANSITIONS: Record<Holiday, Holiday> = {
+        [Holiday.Halloween]: Holiday.Thanksgiving,
+        [Holiday.NewYears]: Holiday.Birthday,
+        [Holiday.Thanksgiving]: Holiday.Hanukkah,
+        [Holiday.Birthday]: Holiday.Halloween,
+        [Holiday.Hanukkah]: Holiday.NewYears,
+    };
+
+    function advanceAlphabet(): void {
+        const newHoliday: Holiday = ALPHABET_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+
+    function advanceYear(): void {
+        const newHoliday: Holiday = YEAR_TRANSITIONS[holiday];
+        setHoliday(newHoliday);
+    }
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <div>
+                <Button onClick={advanceAlphabet}>Advance by Alphabet</Button>
+                <Button onClick={advanceYear}>Advance by Year</Button>
+            </div>
+        </div>
+    );
 }
